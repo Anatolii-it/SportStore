29a4e08ce72f6770992239848170b9280c72389e
Анатолій <t0663261690@gmail.com>
2024-03-31 11:50:23 +0300

    Update SportStore.sql
    trigers

--- SportStore.sql
+++ SportStore.sql
@@ -1,5 +1,32 @@
-CREATE DATABASE SportStore;
-GO
 
 USE SportStore;
 GO
+
+CREATE TRIGGER UpdateProductQuantity
+ON Products
+INSTEAD OF INSERT
+AS
+BEGIN
+    -- якщо товар є додаємо до нього кількість
+    UPDATE p
+    SET p.Quantity = p.Quantity + i.Quantity
+    FROM Products p
+    JOIN inserted i ON p.ProductId = i.ProductId;
+    
+    -- Додаємо новий товар якого в нас ще немає
+    INSERT INTO Products (Name, Category, Quantity, Cost, Manufacturer, SalePrice)
+    SELECT i.Name, i.Category, i.Quantity, i.Cost, i.Manufacturer, i.SalePrice
+    FROM inserted i
+    WHERE NOT EXISTS (SELECT 1 FROM Products WHERE ProductId = i.ProductId);
+END;
+
+
+CREATE TRIGGER ArchiveEmployee
+ON Employees
+AFTER DELETE
+AS
+BEGIN
+    INSERT INTO ArchivedEmployees (EmployeeId, FullName, Position, HireDate, Salary, TerminationDate)
+    SELECT EmployeeId, FullName, Position, HireDate, Salary, GETDATE() AS TerminationDate
+    FROM deleted;
+END;
