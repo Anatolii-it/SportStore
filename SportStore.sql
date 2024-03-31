d4b3028cedfc24f632176759e51012669dcdf5c1
Анатолій <t0663261690@gmail.com>
2024-03-31 12:40:22 +0300

    Update SportStore.sql
    update2

--- SportStore.sql
+++ SportStore.sql
@@ -7,18 +7,28 @@ ON Products
 INSTEAD OF INSERT
 AS
 BEGIN
-    -- якщо товар є додаємо до нього кількість
+    -- Оновлюємо кількість товару, якщо він вже існує
     UPDATE p
     SET p.Quantity = p.Quantity + i.Quantity
     FROM Products p
-    JOIN inserted i ON p.ProductId = i.ProductId;
-    
-    -- Додаємо новий товар якого в нас ще немає
+    JOIN inserted i ON p.Name = i.Name AND p.Manufacturer = i.Manufacturer;
+
+    -- Вставляємо новий товар, якщо його немає
     INSERT INTO Products (Name, Category, Quantity, Cost, Manufacturer, SalePrice)
     SELECT i.Name, i.Category, i.Quantity, i.Cost, i.Manufacturer, i.SalePrice
     FROM inserted i
-    WHERE NOT EXISTS (SELECT 1 FROM Products WHERE ProductId = i.ProductId);
+    WHERE NOT EXISTS (
+        SELECT 1 
+        FROM Products p 
+        WHERE p.Name = i.Name AND p.Manufacturer = i.Manufacturer
+    );
 END;
+
+--Приклад 1 позіція є, зміюється лише кількість. 2 позіції немає
+INSERT INTO Products (Name, Category, Quantity, Cost, Manufacturer, SalePrice)
+VALUES
+    ('Running Shoes', 'Footwear', 50, 99.99, 'Nike', 79.99),
+    ('Basketball_red', 'Sports Equipment', 30, 19.99, 'Spalding', 29.99);
 
 
 CREATE TRIGGER ArchiveEmployee
@@ -30,3 +40,30 @@ BEGIN
     SELECT EmployeeId, FullName, Position, HireDate, Salary, GETDATE() AS TerminationDate
     FROM deleted;
 END;
+
+CREATE TRIGGER PreventProducts
+ON Sales
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        JOIN Products p ON i.ProductId = p.ProductId
+        WHERE p.Name IN ('Яблука', 'Груші', 'Сливи', 'Кінза')
+    )
+    BEGIN
+        RAISERROR ('Продаж товарів "Яблука", "Груші", "Сливи" та "Кінза" заборонений.', 16, 1)
+        ROLLBACK TRANSACTION
+    END
+    ELSE
+    BEGIN
+        INSERT INTO Sales (ProductId, SalePrice, Quantity, SaleDate, CustomerId, EmployeeId)
+        SELECT ProductId, SalePrice, Quantity, SaleDate, CustomerId, EmployeeId
+        FROM inserted;
+    END
+END;
+--приклад заборони
+INSERT INTO Sales (ProductId, SalePrice, Quantity, SaleDate, CustomerId, EmployeeId)
+VALUES
+    (23,  99.99, 10, '2024-03-31', 4,3);
